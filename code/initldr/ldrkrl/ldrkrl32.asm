94913011a6e48cdd5de8da7423eef09982f0a787
0WAQ <2758282037@qq.com>
2024-07-28 11:40:08 +0800

    修改ldrkrl32.asm文件

--- code/initldr/ldrkrl/ldrkrl32.asm
+++ code/initldr/ldrkrl/ldrkrl32.asm
@@ -1,3 +1,13 @@
+%include "../include/base/ldrasm.inc"
+
+global _start
+global realadr_call_entry
+global IDT_PTR
+extern ldrkrl_entry
+
+[section .text]
+[bits 32]
+_start:
 _entry:
     cli
     lgdt [GDT_PTR]                  ;加载GDT地址到GDTR寄存器
