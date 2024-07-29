b9bd5cecd70b28ebacb9e3e3c944ecbc3b477af5
0WAQ <2758282037@qq.com>
2024-07-29 15:22:34 +0800

    修改汇编代码的格式

--- code/initldr/ldrkrl/imginithead.asm
+++ code/initldr/ldrkrl/imginithead.asm
@@ -5,9 +5,10 @@
 
 ;;; GRUB1和GRUB2需要的两个头结构
 ;; 符号定义与全局说明
-MBT_HDR_FLAGS EQU 0x00010003    ;定义多引导标头的标志
-MBT_HDR_MAGIC EQU 0x1BADB002    ;定义多引导标头的魔数, 用于识别多引导信息结构
-MBT2_MAGIC    EQU 0xe85250d6    ;定义多引导2表头的魔数
+        MBT_HDR_FLAGS EQU 0x00010003    ;定义多引导标头的标志
+        MBT_HDR_MAGIC EQU 0x1BADB002    ;定义多引导标头的魔数, 用于识别多引导信息结构
+        MBT2_MAGIC    EQU 0xe85250d6    ;定义多引导2表头的魔数
+
 global _start                   ;声明一个全局符号, 可以被链接器和其它模块访问
 extern inithead_entry           ;声明一个外部符号, 需要在其它地方定义
 
@@ -16,83 +17,89 @@ extern inithead_entry           ;声明一个外部符号, 需要在其它地方
 ;;; 代码段与起始点
 [section .text]                 ;声明代码段为.text段, 通常包含可执行代码
 [bits 32]                       ;声明代码使用32位指令集
+
+
 _start:                         ;_start入口点
-    jmp _entry                  ;跳转到_entry
+        jmp _entry                  ;跳转到_entry
 
 
 
-;;; 对齐和GRUB1  
-align 4                         ;接下来的数据对齐到4字节边界
-mbt_hdr:                        ;定义多引导标头(multiboot header)
-    dd MBT_HDR_MAGIC            ;定义多引导魔数
-    dd MBT_HDR_FLAGS            ;定义多引导标志
-    dd -(MBT_HDR_MAGIC + MBT_HDR_FLAGS)     ;定义检查和, 确保标头的所有字段之和为0
-    dd mbt_hdr                  ;指向多引导标头自身的指针
-    dd _start                   ;指向程序入口_start
-    dd 0                        ;
-    dd 0                        ;其余字段填充为0或者_entry, 具体取决于实现需求
-    dd _entry                   ;
+;;; GRUB1  
+align 4     ;接下来的数据对齐到4字节边界
+
+;定义多引导标头(multiboot header)
+mbt_hdr:
+        dd MBT_HDR_MAGIC            ;定义多引导魔数
+        dd MBT_HDR_FLAGS            ;定义多引导标志
+        dd -(MBT_HDR_MAGIC + MBT_HDR_FLAGS)     ;定义检查和, 确保标头的所有字段之和为0
+        dd mbt_hdr                  ;指向多引导标头自身的指针
+        dd _start                   ;指向程序入口_start
+        dd 0                        ;
+        dd 0                        ;其余字段填充为0或者_entry, 具体取决于实现需求
+        dd _entry                   ;
 
 
 
-;;; 对齐和GRUB2
-ALIGN 8                         ;接下来的数据对齐到8字节边界
-mbhdr:                          ;定义多引导2标头
-    DD 0xE85250D6               ;定义多引导2魔数
-    DD 0                        ;标头的架构字段(0: i386)
-    DD mbhdrend - mbhdr          ;定义标头的总大小
-    DD -(0xE85250D6 + 0 + (mbhdrend - mbhdr))    ;定义检查和, 确保标头的所有字段之和为0
-    DW 2, 0                     ;定义多引导头部的版本为2
-    DD 24                       ;指向头部字段的总大小(除了前8字节)
-    DD mbhdr                    ;提供头部开始的指针
-    DD _start                   ;.text段的开始指针
-    DD 0
-    DD 0
-    DD 3, 0                     ;OS的类型(3: 兼容32和64位)
-    DD 12
-    DD _entry                   ;
-    DD 0
-    DW 0, 0
-    DD 8                        ;最后一个标签的大小
+;;; GRUB2
+align 8     ;接下来的数据对齐到8字节边界
+
+;定义多引导2标号
+mbhdr:
+        dd 0xE85250D6               ;定义多引导2魔数
+        dd 0                        ;标头的架构字段(0: i386)
+        dd mbhdrend - mbhdr          ;定义标头的总大小
+        dd -(0xE85250D6 + 0 + (mbhdrend - mbhdr))    ;定义检查和, 确保标头的所有字段之和为0
+        dw 2, 0                     ;定义多引导头部的版本为2
+        dd 24                       ;指向头部字段的总大小(除了前8字节)
+        dd mbhdr                    ;提供头部开始的指针
+        dd _start                   ;.text段的开始指针
+        dd 0
+        dd 0
+        dd 3, 0                     ;OS的类型(3: 兼容32和64位)
+        dd 12
+        dd _entry                   ;
+        dd 0
+        dw 0, 0
+        dd 8                        ;最后一个标签的大小
 mbhdrend:
 
 
 ;;;入口点与保护模式初始化
 ;;关中断并加载GDT
 _entry:
-    cli                         ;禁止中断
+        cli                         ;禁止中断
 
-    ;; 设置RTC寄存器   
-    in al, 0x70
-    or al, 0x80
-    out 0x70, al                ;关闭不可屏蔽中断
+        ;; 设置RTC寄存器   
+        in al, 0x70
+        or al, 0x80
+        out 0x70, al                ;关闭不可屏蔽中断
 
-    lgdt [GDT_PTR]              ;加载GDT地址到GDTR寄存器
-    jmp dword 0x8 :_32bits_mode ;长跳转刷新CS影子寄存器
+        lgdt [GDT_PTR]              ;加载GDT地址到GDTR寄存器
+        jmp dword 0x8 :_32bits_mode ;长跳转刷新CS影子寄存器
 
 
 ;; 初始化 段寄存器, 通用寄存器, 栈寄存器, 为了调用inithead_entry这个C函数做准备
 _32bits_mode:
-    mov ax, 0x10
-    mov ds, ax
-    mov ss, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
+        mov ax, 0x10
+        mov ds, ax
+        mov ss, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
 
-    xor eax, eax
-    xor ebx, ebx
-    xor ecx, ecx
-    xor edx, edx
+        xor eax, eax
+        xor ebx, ebx
+        xor ecx, ecx
+        xor edx, edx
     
-    xor edi, edi
-    xor esi, esi
-    xor ebp, ebp
-    xor esp, esp
+        xor edi, edi
+        xor esi, esi
+        xor ebp, ebp
+        xor esp, esp
     
-    mov esp, 0x7c00             ;设置堆栈指针, BIOS引导扇区加载到内存的地方
-    call inithead_entry         ;调用外部函数, 去初始化内核
-    jmp 0x200000                ;跳转到地址, 开始执行内核代码
+        mov esp, 0x7c00             ;设置堆栈指针, BIOS引导扇区加载到内存的地方
+        call inithead_entry         ;调用外部函数, 去初始化内核
+        jmp 0x200000                ;跳转到地址, 开始执行内核代码
 
 
 ;; GDT全局段描述符表
--- code/initldr/ldrkrl/ldrkrl32.asm
+++ code/initldr/ldrkrl/ldrkrl32.asm
@@ -9,67 +9,67 @@ extern ldrkrl_entry
 [bits 32]
 _start:
 _entry:
-    cli
-    lgdt [GDT_PTR]                  ;加载GDT地址到GDTR寄存器
-    lidt [LDT_PTR]                  ;假造LDT地址到LDTR寄存器
-    jmp dword 0x8 :_32bits_mode     ;长跳转影子寄存器
+        cli
+        lgdt [GDT_PTR]                  ;加载GDT地址到GDTR寄存器
+        lidt [LDT_PTR]                  ;假造LDT地址到LDTR寄存器
+        jmp dword 0x8 :_32bits_mode     ;长跳转影子寄存器
 
 
 ;;初始化相关寄存器
 _32bits_mode:
-    mov ax, 0x10                    ;数据段选择子(目的)
-    mov ds, ax
-    mov ss, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
+        mov ax, 0x10                    ;数据段选择子(目的)
+        mov ds, ax
+        mov ss, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
 
-    xor eax, eax
-    xor ebx, ebx
-    xor ecx, ecx
-    xor edx, edx
-    xor edi, edi
-    xor esi, esi
-    xor ebp, ebp
-    xor esp, esp
+        xor eax, eax
+        xor ebx, ebx
+        xor ecx, ecx
+        xor edx, edx
+        xor edi, edi
+        xor esi, esi
+        xor ebp, ebp
+        xor esp, esp
 
-    mov esp, 0x90000                ;栈底指针指向0x90000
+        mov esp, 0x90000                ;栈底指针指向0x90000
 
-    call ldrkrl_entry               ;调用ldrkrl_entry函数
-    xor ebx, ebx                    ;清空ebx寄存器
+        call ldrkrl_entry               ;调用ldrkrl_entry函数
+        xor ebx, ebx                    ;清空ebx寄存器
 
-    jmp 0x2000000
-    jmp $
+        jmp 0x2000000
+        jmp $
 
 
 
 ;;;
 realadr_call_entry:
-    pushad                          ;保存通用寄存器
-    push    ds                      ;
-    push    es                      ;保存4个段寄存器
-    push    fs                      ;
-    push    gs                      ;
-    call save_eip_jmp
-    pop     gs                      ;
-    pop     fs                      ;恢复4个段寄存器
-    pop     es                      ;
-    pop     ds                      ;
-    popad                           ;恢复通用寄存器
-    ret
+        pushad                          ;保存通用寄存器
+        push    ds                      ;
+        push    es                      ;保存4个段寄存器
+        push    fs                      ;
+        push    gs                      ;
+        call save_eip_jmp
+        pop     gs                      ;
+        pop     fs                      ;恢复4个段寄存器
+        pop     es                      ;
+        pop     ds                      ;
+        popad                           ;恢复通用寄存器
+        ret
 
 ;
 save_eip_jmp:
-    pop esi                         ;弹出调用save_eip_jmp时保存的eip到esp中
-    mov [PM32_EIP_OFF], esi         ;将eip保存到特定的内存空间中
-    mov [PM32_ESP_OFF], esp         ;将esp保存到特定的内存空间中
-    jmp dword far [cpmty_mode]      ;长跳转, 将cpmty_mode处的第一个4字节装入eip, 把随后的2字节装入cs
+        pop esi                         ;弹出调用save_eip_jmp时保存的eip到esp中
+        mov [PM32_EIP_OFF], esi         ;将eip保存到特定的内存空间中
+        mov [PM32_ESP_OFF], esp         ;将esp保存到特定的内存空间中
+        jmp dword far [cpmty_mode]      ;长跳转, 将cpmty_mode处的第一个4字节装入eip, 把随后的2字节装入cs
 
 ;
 cpmty_mode:
-    dd 0x1000
-    dw 0x18
-    jmp $
+        dd 0x1000
+        dw 0x18
+        jmp $
 
 
 
