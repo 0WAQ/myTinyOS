bf45ab401193d7da2f08107b9519023f5304354f
0WAQ <2758282037@qq.com>
2024-09-05 15:09:31 +0800

    lmkfbuild.c

--- script/lmkfbuild.c
+++ script/lmkfbuild.c
@@ -4,69 +4,68 @@
 
 char* retn_ifilenm(int gc,char* argv[])
 {
-	if(gc<1||NULL==argv)
-	{
+	if(gc < 1 || argv == NULL) {
 		return NULL;
 	}
-	for(int i=0;i<gc;i++)
-	{
-		if(strcmp("-i",argv[i])==0)
-		{
-			if(i<(gc-1))
-			{
-				return argv[i+1];
+
+	for(int i = 0; i < gc; i++) {
+		if(strcmp("-i", argv[i]) == 0) {
+			if(i <gc - 1) {
+				return argv[i + 1];
 			}
 		}
 	}
+
 	return NULL;
 }
 
 char* retn_ofilenm(int gc,char * argv[])
 {
-	if(gc<1||NULL==argv)
-	{
+	if(gc < 1 || argv == NULL) {
 		return NULL;
 	}
-	for(int i=0;i<gc;i++)
-	{
-		if(strcmp("-o",argv[i])==0)
-		{
-			if(i<(gc-1))
-			{
-				return argv[i+1];
+
+	for(int i = 0; i < gc; i++) {
+		if(strcmp("-o", argv[i]) == 0) {
+			if(i < gc - 1) {
+				return argv[i + 1];
 			}
 		}
 	}
+
 	return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-	if(argc<4)
-	{
+	if(argc<4) {
 		printf("pram argc:%d\n", argc);
 		exit(1);
 	}
-	char* ifnm=retn_ifilenm(argc,argv);
-	char* ofnm=retn_ofilenm(argc,argv);
-	FILE* ifp=fopen(ifnm,"r");
-    FILE* ofp=fopen(ofnm,"w+");
-    if (NULL==ifp||NULL==ofp)
-    {
-    	printf("open fail:%s:%s\n", ifnm,ofnm);
+
+	char* ifnm = retn_ifilenm(argc,argv);
+	char* ofnm = retn_ofilenm(argc,argv);
+	FILE* ifp = fopen(ifnm,"r");
+    FILE* ofp = fopen(ofnm,"w+");
+    
+	if(ifp == NULL || ofp == NULL) {
+    	printf("open fail: %s:%s\n", ifnm, ofnm);
     	exit(1);
     }
-	char fch=fgetc(ifp); 
-	while(fch!=EOF)
+
+	char fch = fgetc(ifp); 
+	while(fch != EOF)
     {
-    	if('['==fch)
-    	{
-    		fch='\t';
+    	if(fch == '[') {
+    		fch = '\t';
     	}
-        fputc(fch,ofp);
-        fch=fgetc(ifp);
-    } 
+
+        fputc(fch, ofp);
+        fch = fgetc(ifp);
+	}
+
     fclose(ifp); 
     fclose(ofp);    
+	
 	return 0;
 }
