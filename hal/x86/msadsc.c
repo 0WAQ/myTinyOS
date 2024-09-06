e9c2e4292cd12d35e9780b131a1236d45a49301c
0WAQ <2758282037@qq.com>
2024-09-06 22:30:48 +0800

    初始化内存页结构

--- hal/x86/msadsc.c
+++ hal/x86/msadsc.c
@@ -1,15 +1,92 @@
 /**********************************************************
         物理内存空间数组文件msadsc.c
-***********************************************************
-                彭东
 **********************************************************/
 #include "cosmostypes.h"
 #include "cosmosmctrl.h"
 
+// 初始化内存页结构(即初始化msdasc_t结构体中的变量)
+void init_msadsc()
+{
+	u64_t coremdnr = 0;
+	u64_t msadscnr = 0;					// 数组元素个数
+	msadsc_t *msadscvp = NULL;			// 数组首地址
+	machbstart_t *mbsp = &kmachbsp;
+
+// 计算msadsc_t数组的开始地址和数组元素个数
+	if (ret_msadsc_vadrandsz(mbsp, &msadscvp, &msadscnr) == FALSE)
+		system_error("init_msadsc ret_msadsc_vadrandsz err\n");
+
+// 初始化msadsc_t数组
+	coremdnr = init_msadsc_core(mbsp, msadscvp, msadscnr);
+	if (coremdnr != msadscnr)
+		system_error("init_msadsc init_msadsc_core err\n");
+
+// 将msadsc_t数组的相关信息写入全局machbstart中
+
+	// 开始物理地址
+	mbsp->mb_memmappadr = viradr_to_phyadr((adr_t)msadscvp);
+
+	// 元素个数
+	mbsp->mb_memmapnr = coremdnr;
+	
+	// 数组大小
+	mbsp->mb_memmapsz = coremdnr * sizeof(msadsc_t);
+	
+	// 计算下一个空闲内存的起始地址
+	mbsp->mb_nextwtpadr = PAGE_ALIGN(mbsp->mb_memmappadr + mbsp->mb_memmapsz);
+}
+
+u64_t init_msadsc_core(machbstart_t *mbsp, msadsc_t *msavstart, u64_t msanr)
+{
+	/**
+	 * 扫描 phymmarge_t 数组, 若内存可用, 就建立一个msadsc_t
+	 */
+
+	// 获取phymmarge_t数组
+	phymmarge_t *pmagep = (phymmarge_t *)phyadr_to_viradr((adr_t)mbsp->mb_e820expadr);
+
+	u64_t mdidx = 0;
+
+	// 遍历phymmarge数组
+	for (u64_t i = 0; i < mbsp->mb_e820exnr; i++)
+	{
+		// 判断是不是可用内存
+		if (pmagep[i].pmr_type == PMR_T_OSAPUSERRAM)
+		{
+			// 遍历这一块地址
+			for (u64_t start = pmagep[i].pmr_saddr; start < pmagep[i].pmr_end; start += 4096)
+			{
+				// 若这块地址大小足够 1 个页(4KB), 就创建一个msadsc
+				if (start + 4096 - 1 <= pmagep[i].pmr_end)
+				{
+					write_one_msadsc(&msavstart[mdidx], start);
+					mdidx++;
+				}
+			}
+		}
+	}
+
+	return mdidx;
+}
+
+// 创建一个msadsc
+void write_one_msadsc(msadsc_t *msap, u64_t phyadr)
+{
+	// 初始化msadsc
+	msadsc_t_init(msap);
+
+	// 将64位物理地址转换为phyadrflgs
+	phyadrflgs_t *tmp = (phyadrflgs_t *)(&phyadr);
+
+	msap->md_phyadrs.paf_padrs = tmp->paf_padrs;
+}
+
 void msadsc_t_init(msadsc_t *initp)
 {
+	// 初始化链表和锁
 	list_init(&initp->md_list);
 	knl_spinlock_init(&initp->md_lock);
+
 	initp->md_indxflgs.mf_olkty = MF_OLKTY_INIT;
 	initp->md_indxflgs.mf_lstty = MF_LSTTY_LIST;
 	initp->md_indxflgs.mf_mocty = MF_MOCTY_FREE;
@@ -28,20 +105,16 @@ void msadsc_t_init(msadsc_t *initp)
 	initp->md_odlink = NULL;
 }
 
-void disp_one_msadsc(msadsc_t *mp)
-{
-	kprint("msadsc_t.md_f:_ux[%x],_my[%x],md_phyadrs:_alc[%x],_shd[%x],_swp[%x],_che[%x],_kmp[%x],_lck[%x],_dty[%x],_bsy[%x],_padrs[%x]\n",
-		   (uint_t)mp->md_indxflgs.mf_uindx, (uint_t)mp->md_indxflgs.mf_mocty, (uint_t)mp->md_phyadrs.paf_alloc, (uint_t)mp->md_phyadrs.paf_shared, (uint_t)mp->md_phyadrs.paf_swap, (uint_t)mp->md_phyadrs.paf_cache, (uint_t)mp->md_phyadrs.paf_kmap, (uint_t)mp->md_phyadrs.paf_lock,
-		   (uint_t)mp->md_phyadrs.paf_dirty, (uint_t)mp->md_phyadrs.paf_busy, (uint_t)(mp->md_phyadrs.paf_padrs << 12));
-}
-
+// 通过machbstart计算msadsc_t数组的起始地址和元素个数
 bool_t ret_msadsc_vadrandsz(machbstart_t *mbsp, msadsc_t **retmasvp, u64_t *retmasnr)
 {
 	if (NULL == mbsp || NULL == retmasvp || NULL == retmasnr) {
 		return FALSE;
 	}
 
-	if (mbsp->mb_e820exnr < 1 || NULL == mbsp->mb_e820expadr || (mbsp->mb_e820exnr * sizeof(phymmarge_t)) != mbsp->mb_e820exsz)
+	if (mbsp->mb_e820exnr < 1 || 
+		mbsp->mb_e820expadr == NULL || 
+		mbsp->mb_e820exnr * sizeof(phymmarge_t) != mbsp->mb_e820exsz)
 	{
 		*retmasvp = NULL;
 		*retmasnr = 0;
@@ -66,7 +139,7 @@ bool_t ret_msadsc_vadrandsz(machbstart_t *mbsp, msadsc_t **retmasvp, u64_t *retm
 		return FALSE;
 	}
 
-	//msadnr=usrmemsz>>12;
+	// msadnr = usrmemsz >> 12;
 	if (0 != initchkadr_is_ok(mbsp, mbsp->mb_nextwtpadr, (msadnr * sizeof(msadsc_t)))) {
 		system_error("ret_msadsc_vadrandsz initchkadr_is_ok err\n");
 	}
@@ -77,52 +150,11 @@ bool_t ret_msadsc_vadrandsz(machbstart_t *mbsp, msadsc_t **retmasvp, u64_t *retm
 	return TRUE;
 }
 
-void write_one_msadsc(msadsc_t *msap, u64_t phyadr)
+void disp_one_msadsc(msadsc_t *mp)
 {
-	msadsc_t_init(msap);
-	phyadrflgs_t *tmp = (phyadrflgs_t *)(&phyadr);
-	msap->md_phyadrs.paf_padrs = tmp->paf_padrs;
-}
-
-u64_t init_msadsc_core(machbstart_t *mbsp, msadsc_t *msavstart, u64_t msanr)
-{
-	phymmarge_t *pmagep = (phymmarge_t *)phyadr_to_viradr((adr_t)mbsp->mb_e820expadr);
-
-	u64_t mdindx = 0;
-	for (u64_t i = 0; i < mbsp->mb_e820exnr; i++)
-	{
-		if (PMR_T_OSAPUSERRAM == pmagep[i].pmr_type)
-		{
-			for (u64_t start = pmagep[i].pmr_saddr; start < pmagep[i].pmr_end; start += 4096)
-			{
-				if ((start + 4096 - 1) <= pmagep[i].pmr_end)
-				{
-					write_one_msadsc(&msavstart[mdindx], start);
-					mdindx++;
-				}
-			}
-		}
-	}
-
-	return mdindx;
-}
-
-void init_msadsc()
-{
-	u64_t coremdnr = 0, msadscnr = 0;
-	msadsc_t *msadscvp = NULL;
-	machbstart_t *mbsp = &kmachbsp;
-	if (ret_msadsc_vadrandsz(mbsp, &msadscvp, &msadscnr) == FALSE)
-		system_error("init_msadsc ret_msadsc_vadrandsz err\n");
-
-	coremdnr = init_msadsc_core(mbsp, msadscvp, msadscnr);
-	if (coremdnr != msadscnr)
-		system_error("init_msadsc init_msadsc_core err\n");
-
-	mbsp->mb_memmappadr = viradr_to_phyadr((adr_t)msadscvp);
-	mbsp->mb_memmapnr = coremdnr;
-	mbsp->mb_memmapsz = coremdnr * sizeof(msadsc_t);
-	mbsp->mb_nextwtpadr = PAGE_ALIGN(mbsp->mb_memmappadr + mbsp->mb_memmapsz);
+	kprint("msadsc_t.md_f:_ux[%x],_my[%x],md_phyadrs:_alc[%x],_shd[%x],_swp[%x],_che[%x],_kmp[%x],_lck[%x],_dty[%x],_bsy[%x],_padrs[%x]\n",
+		   (uint_t)mp->md_indxflgs.mf_uindx, (uint_t)mp->md_indxflgs.mf_mocty, (uint_t)mp->md_phyadrs.paf_alloc, (uint_t)mp->md_phyadrs.paf_shared, (uint_t)mp->md_phyadrs.paf_swap, (uint_t)mp->md_phyadrs.paf_cache, (uint_t)mp->md_phyadrs.paf_kmap, (uint_t)mp->md_phyadrs.paf_lock,
+		   (uint_t)mp->md_phyadrs.paf_dirty, (uint_t)mp->md_phyadrs.paf_busy, (uint_t)(mp->md_phyadrs.paf_padrs << 12));
 }
 
 void disp_phymsadsc()
