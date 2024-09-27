6ae5d8ff3ed3582068ece8878cc636847e5b5a75
0WAQ <2758282037@qq.com>
2024-09-27 22:20:36 +0800

    lib

---
+++ lib/lapimm.c
@@ -0,0 +1,19 @@
+/**********************************************************
+        COSMOS内存管理API文件lapimm.c
+**********************************************************/
+#include "libtypes.h"
+#include "libheads.h"
+
+void* api_mallocblk(size_t blksz)
+{
+    void* retadr;
+    API_ENTRY_PARE1(INR_MM_ALLOC, retadr, blksz);
+    return retadr;
+}
+
+sysstus_t api_mfreeblk(void* fradr,size_t blksz)
+{
+    sysstus_t retstus;
+    API_ENTRY_PARE2(INR_MM_FREE, retstus, fradr, blksz);
+    return retstus;
+}
