3f99d401272bf2de2f4fb0864041ddc228ec017d
0WAQ <2758282037@qq.com>
2024-09-30 17:58:51 +0800

    initldr/ldrkrl/bstartparm.c

--- initldr/ldrkrl/bstartparm.c
+++ initldr/ldrkrl/bstartparm.c
@@ -36,7 +36,7 @@ void init_bstartparm()
     // 设置图形模式
     init_graph(mbsp);
 
-    return;
+    die(0x400);
 }
 
 // 初始化machbstart_t结构体, 清0, 并设置标志
@@ -44,7 +44,6 @@ void machbstart_t_init(machbstart_t* initp)
 {
     memset(initp, 0, sizeof(machbstart_t));
     initp->mb_migc = MBS_MIGC;
-    return;
 }
 
 int adrzone_is_ok(u64_t sadr, u64_t slen, u64_t kadr, u64_t klen)
@@ -84,4 +83,46 @@ int chkadr_is_ok(machbstart_t *mbsp, u64_t chkadr, u64_t cksz)
         return -7;
 
     return 0;
+}
+
+void disp_mbsp(machbstart_t *mbsp)
+{
+    kprint("mbsp->mb_imgpadr:%x\n", mbsp->mb_imgpadr);
+    kprint("mbsp->mb_imgsz:%x\n", mbsp->mb_imgsz);
+    kprint("mbsp->mb_krlimgpadr:%x\n", mbsp->mb_krlimgpadr);
+    kprint("mbsp->mb_krlimgsz:%x\n", mbsp->mb_krlsz);
+    kprint("mbsp->mb_e820padr:%x\n", mbsp->mb_e820padr);
+    kprint("mbsp->mb_e820nr:%x\n", mbsp->mb_e820nr);
+    kprint("mbsp->mb_e820sz:%x\n", mbsp->mb_e820sz);
+    kprint("mbsp->mb_nextwtpadr:%x\n", mbsp->mb_nextwtpadr);
+    kprint("mbsp->mb_kalldendpadr:%x\n", mbsp->mb_kalldendpadr);
+    kprint("mbsp->mb_pml4padr:%x\n", mbsp->mb_pml4padr);
+    kprint("mbsp->mb_subpageslen:%x\n", mbsp->mb_subpageslen);
+    kprint("mbsp->mb_kpmapphymemsz:%x\n", mbsp->mb_kpmapphymemsz);
+    kprint("mbsp->mb_cpumode:%x\n", mbsp->mb_cpumode);
+    kprint("mbsp->mb_memsz:%x\n", mbsp->mb_memsz);
+    kprint("mbsp->mb_krlinitstack:%x\n", mbsp->mb_krlinitstack);
+    kprint("mbsp->mb_krlitstacksz:%x\n", mbsp->mb_krlitstacksz);
+    kprint("mbsp->mb_bfontpadr:%x\n", mbsp->mb_bfontpadr);
+    kprint("mbsp->mb_bfontsz:%x\n", mbsp->mb_bfontsz);
+
+    die(0);
+}
+
+void testm2m()
+{
+    u8_t *s = (u8_t *)0x400000;
+    u8_t *d = (u8_t *)(0x400000 - 1);
+    for (u8_t i = 0; i < 10; i++)
+        s[i] = i;
+
+    for (u8_t j = 0; j < 10; j++)
+        kprint("s[%d]:%d\n", j, s[j]);
+
+    m2mcopy(s, d, 10);
+    
+    for (u8_t k = 0; k < 10; k++)
+        kprint("d[%d]:%d\n", k, d[k]);
+
+    die(0);
 }
